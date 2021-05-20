0a1873e82bb5279fc42840b5f00e42020ebcfbf2
NotFlyingBullet <pavel79@yandex.ru>
2021-05-20 21:06:27 +0600

    04 input checkbox textarea

---
+++ 04 Input range textaria checkbox/js/script.js
@@ -0,0 +1,34 @@
+//Комментарий
+/* 
+document.querySelector('button').onclick = ()=>{
+  console.log(document.querySelector('#one').value)
+document.querySelector('button').style.backgroundColor=document.querySelector('#one').value;
+}
+document.querySelector('#one').oninput = ()=>{
+  console.log(document.querySelector('#one').value);
+  document.querySelector('span').innerHTML = document.querySelector('#one').value;
+}
+*/
+
+/*
+ document.querySelector('#btn-1').onclick = ()=>{
+  console.log(document.querySelector('#i2').value);
+  let myCheckbox = document.querySelector('#i2');
+  console.log(myCheckbox.checked);
+  if (myCheckbox.checked){
+    console.log("Нажат");
+  }else{
+    console.log("Не нажат");
+  }
+} 
+*/
+document.querySelector('#btn-2').onclick = (event)=>{
+  event.preventDefault();
+  /* let text = document.querySelector('#two');
+  console.log(text.value);
+  text.value = "Принято!"; */
+  let form = document.querySelector('form');
+  //console.log(form);
+  console.log(form.elements.two.value);
+  console.log(form.elements.n2.value);
+}
